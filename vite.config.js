c55a04e1624141ba11ac3d422cfa2be22277518c
alagunandhini <alagunadhini@gmail.com>
2025-12-27 12:01:41 +0530

    Prepare for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,7 @@ export default defineConfig({
   plugins: [react(),
         tailwindcss(),
   ],
+   optimizeDeps: {
+    include: ['lucide-react'], // ensures lucide-react works on Vercel
+  },
 })
